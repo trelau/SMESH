d2032a03907208233a44359d01db29bd341775da
Trevor Laughlin <trevor@laughlinresearch.com>
2017-10-26 22:11:47 -0400

    Use CreateFileA() and GetFileAttributesA()

--- src/SMESH/SMESH_File.cxx
+++ src/SMESH/SMESH_File.cxx
@@ -79,8 +79,8 @@ bool SMESH_File::open()
   if ( !_map && length > 0 )
   {
 #ifdef WIN32
-    _file = CreateFile(_name.data(), GENERIC_READ, FILE_SHARE_READ,
-                       NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+    _file = CreateFileA(_name.data(), GENERIC_READ, FILE_SHARE_READ,
+                        NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
     bool ok = ( _file != INVALID_HANDLE_VALUE );
 #else
     _file = ::open(_name.data(), O_RDONLY );
@@ -292,13 +292,13 @@ bool SMESH_File::openForWriting()
 {
 #ifdef WIN32
 
-  _file = CreateFile( _name.c_str(),          // name of the write
-                      GENERIC_WRITE,          // open for writing
-                      0,                      // do not share
-                      NULL,                   // default security
-                      OPEN_ALWAYS,            // CREATE NEW or OPEN EXISTING
-                      FILE_ATTRIBUTE_NORMAL,  // normal file
-                      NULL);                  // no attr. template
+  _file = CreateFileA( _name.c_str(),          // name of the write
+                       GENERIC_WRITE,          // open for writing
+                       0,                      // do not share
+                       NULL,                   // default security
+                       OPEN_ALWAYS,            // CREATE NEW or OPEN EXISTING
+                       FILE_ATTRIBUTE_NORMAL,  // normal file
+                       NULL);                  // no attr. template
   return ( _file != INVALID_HANDLE_VALUE );
 
 #else
