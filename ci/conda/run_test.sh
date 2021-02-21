6337c390f4762d00c55ba2048ea61f38b0ca7a25
Trevor Laughlin <trevor.w.laughlin@gmail.com>
2021-02-21 14:05:43 -0500

    Optionally build and test Netgen and add a simple test to make sure catch is working at all

--- ci/conda/run_test.sh
+++ ci/conda/run_test.sh
@@ -7,15 +7,25 @@ if [[ ${HOST} =~ .*linux.* ]]; then
     CMAKE_PLATFORM_FLAGS+=(-DCMAKE_TOOLCHAIN_FILE="${RECIPE_DIR}/cross-linux.cmake")
 fi
 
+declare ENABLE_NETGEN=true
+if [[ ${HOST} =~ .*apple.* ]]; then
+  ENABLE_NETGEN=true  # set to false if you don't want to build and test Netgen
+fi
+
 cmake -G "Ninja" \
       -D CMAKE_BUILD_TYPE:STRING="Release" \
       ${CMAKE_PLATFORM_FLAGS[@]} \
       -D CMAKE_INSTALL_PREFIX:FILEPATH=$PREFIX \
       -D CMAKE_PREFIX_PATH:FILEPATH=$PREFIX \
+      -D ENABLE_NETGEN:BOOL=$ENABLE_NETGEN \
       ..
 
 ninja install
 
 cd ..
 cd tests
-./test_SMESH
+./test_Catch
+./test_StdMeshers
+if [ $ENABLE_NETGEN ] ; then
+    ./test_NETGENPlugin
+fi
