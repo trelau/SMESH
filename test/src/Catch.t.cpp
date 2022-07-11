529e5f95147af436df69dde3f3e64849515e685f
looooo <sppedflyer@gmail.com>
2022-07-11 15:45:41 +0200

    revert catch2 changes

--- test/src/Catch.t.cpp
+++ test/src/Catch.t.cpp
@@ -1,6 +1,6 @@
 #define CATCH_CONFIG_MAIN
 
-#include <catch2/catch_all.hpp>
+#include <catch2/catch.hpp>
 
 TEST_CASE("Does catch work at all?",) {
 
--- test/src/NETGENPlugin.t.cpp
+++ test/src/NETGENPlugin.t.cpp
@@ -1,6 +1,6 @@
 #define CATCH_CONFIG_MAIN
 
-#include <catch2/catch_all.hpp>
+#include <catch2/catch.hpp>
 
 #include <gp_Pnt.hxx>
 #include <gp_Dir.hxx>
